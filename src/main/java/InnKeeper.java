ff7ac936bd21eb75caf8b82e2dd2ebf8a054ea82
Bolin <Bolin8621@gmail.com>
2025-01-17 19:35:17 +0800

    Added initial skeleton, which contains only greetings.

--- src/main/java/InnKeeper.java
+++ src/main/java/InnKeeper.java
@@ -1,7 +1,8 @@
 public class InnKeeper {
     public static void main(String[] args) {
         // Generated with https://patorjk.com/software/taag/
-        // Font: "Big"
+        // Font: "Big" (The font is called "Big" on the website)
+        // Name: InnKeeper
         String logo = " _____             _  __\n"
                     + "|_   _|           | |/ /                         \n"
                     + "  | |  _ __  _ __ | ' / ___  ___ _ __   ___ _ __ \n"
@@ -11,5 +12,13 @@ public class InnKeeper {
                     + "                                | |              \n"
                     + "                                |_|              \n";
         System.out.println("Hello from\n" + logo);
+
+        final String lineSeparator = "____________________________________________________________";
+        System.out.println(lineSeparator);
+        System.out.println("Hello! I'm InnKeeper");
+        System.out.println("What can I do for you?");
+        System.out.println(lineSeparator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(lineSeparator);
     }
 }
