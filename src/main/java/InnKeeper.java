af03c0fa9a21b8fa5a9dd6eed01b94d853fee968
Bolin <Bolin8621@gmail.com>
2025-01-17 19:40:59 +0800

    Cleaned up greetings by grouping into functions.

--- src/main/java/InnKeeper.java
+++ src/main/java/InnKeeper.java
@@ -1,23 +1,34 @@
 public class InnKeeper {
+    static final String lineSeparator = "____________________________________________________________";
+
     public static void main(String[] args) {
+        printGreetings();
+
+        printFarewell();
+    }
+
+    public static void printGreetings() {
         // Generated with https://patorjk.com/software/taag/
         // Font: "Big" (The font is called "Big" on the website)
         // Name: InnKeeper
         String logo = " _____             _  __\n"
-                    + "|_   _|           | |/ /                         \n"
-                    + "  | |  _ __  _ __ | ' / ___  ___ _ __   ___ _ __ \n"
-                    + "  | | | '_ \\| '_ \\|  < / _ \\/ _ \\ '_ \\ / _ \\ '__|\n"
-                    + " _| |_| | | | | | | . \\  __/  __/ |_) |  __/ |   \n"
-                    + "|_____|_| |_|_| |_|_|\\_\\___|\\___| .__/ \\___|_|   \n"
-                    + "                                | |              \n"
-                    + "                                |_|              \n";
+                + "|_   _|           | |/ /                         \n"
+                + "  | |  _ __  _ __ | ' / ___  ___ _ __   ___ _ __ \n"
+                + "  | | | '_ \\| '_ \\|  < / _ \\/ _ \\ '_ \\ / _ \\ '__|\n"
+                + " _| |_| | | | | | | . \\  __/  __/ |_) |  __/ |   \n"
+                + "|_____|_| |_|_| |_|_|\\_\\___|\\___| .__/ \\___|_|   \n"
+                + "                                | |              \n"
+                + "                                |_|              \n";
         System.out.println("Hello from\n" + logo);
 
-        final String lineSeparator = "____________________________________________________________";
         System.out.println(lineSeparator);
         System.out.println("Hello! I'm InnKeeper");
         System.out.println("What can I do for you?");
         System.out.println(lineSeparator);
+    }
+
+    // function to end the program
+    public static void printFarewell() {
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(lineSeparator);
     }
