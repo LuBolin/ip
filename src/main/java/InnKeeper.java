436787c1fee06d2485162f9e3e0838b684a3784b
Bolin <Bolin8621@gmail.com>
2025-01-17 20:03:16 +0800

    Added support for list.

--- src/main/java/InnKeeper.java
+++ src/main/java/InnKeeper.java
@@ -1,17 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InnKeeper {
-    static final String lineSeparator = "____________________________________________________________";
+    static final String LINE_SEPARATOR = "____________________________________________________________";
+    static final int MAX_LIST_SIZE = 100;
+    static List<String> UserInputList = new ArrayList<String>(MAX_LIST_SIZE);
+
 
     public static void main(String[] args) {
         printGreetings();
 
-        echoUserInput();
+        handleUserInput();
 
         printFarewell();
     }
 
-    public static void printGreetings() {
+    static void printGreetings() {
         // Generated with https://patorjk.com/software/taag/
         // Font: "Big" (The font is called "Big" on the website)
         // Name: InnKeeper
@@ -25,29 +30,67 @@ public class InnKeeper {
                 + "                                |_|              \n";
         System.out.println("Hello from\n" + logo);
 
-        System.out.println(lineSeparator);
+        System.out.println(LINE_SEPARATOR);
         System.out.println("Greetings! I'm the InnKeeper.");
-        System.out.println("Fancy a beer?");
-        System.out.println(lineSeparator);
+        System.out.println("I can keep track of things for you.");
+        System.out.println(LINE_SEPARATOR);
         // Print an empty line for better readability of user input
         System.out.println();
     }
 
-    public static void printFarewell() {
-        System.out.println(lineSeparator);
+    static void printFarewell() {
+        System.out.println(LINE_SEPARATOR);
         System.out.println("Farewell, traveller!");
-        System.out.println(lineSeparator);
+        System.out.println(LINE_SEPARATOR);
     }
 
-    public static void echoUserInput() {
+    static void handleUserInput() {
         Scanner sc = new Scanner(System.in);
         String userInput = sc.nextLine();
-        while (!userInput.equals("bye")) {
-            System.out.println(lineSeparator);
-            System.out.println(userInput);
-            System.out.println(lineSeparator);
+        while (true){
+            switch (userInput) {
+                case "list":
+                    printList();
+                    break;
+                case "bye":
+                    return;
+                default: // Not a special command, add to list
+                    try {
+                        addToList(userInput);
+                    } catch (ListFullException e) {
+                        System.out.println(LINE_SEPARATOR);
+                        System.out.println(e.getMessage());
+                        System.out.println(LINE_SEPARATOR);
+                    }
+                    break;
+            }
             userInput = sc.nextLine();
         }
     }
 
+    static void addToList(String userInput) throws ListFullException{
+        if (UserInputList.size() >= MAX_LIST_SIZE) {
+            throw new ListFullException("My notebook is full! I can't add any more notes.");
+        }
+        UserInputList.add(userInput);
+        System.out.println(LINE_SEPARATOR);
+        System.out.println("added: " + userInput);
+        System.out.println(LINE_SEPARATOR);
+        System.out.println();
+    }
+
+    static void printList() {
+        System.out.println(LINE_SEPARATOR);
+        for (int i = 0; i < UserInputList.size(); i++) {
+            System.out.println((i + 1) + ". " + UserInputList.get(i));
+        }
+        System.out.println(LINE_SEPARATOR);
+        System.out.println();
+    }
+
+    public static class ListFullException extends Exception {
+        public ListFullException(String message) {
+            super(message);
+        }
+    }
 }
