b7fcef698d9460bda5f13bee12a04d6977ce4909
Bolin <Bolin8621@gmail.com>
2025-01-17 19:30:46 +0800

    Renamed chat bot from Duke to InnKeeper. Renamed main class accordingly.

---
+++ src/main/java/InnKeeper.java
@@ -0,0 +1,15 @@
+public class InnKeeper {
+    public static void main(String[] args) {
+        // Generated with https://patorjk.com/software/taag/
+        // Font: "Big"
+        String logo = " _____             _  __\n"
+                    + "|_   _|           | |/ /                         \n"
+                    + "  | |  _ __  _ __ | ' / ___  ___ _ __   ___ _ __ \n"
+                    + "  | | | '_ \\| '_ \\|  < / _ \\/ _ \\ '_ \\ / _ \\ '__|\n"
+                    + " _| |_| | | | | | | . \\  __/  __/ |_) |  __/ |   \n"
+                    + "|_____|_| |_|_| |_|_|\\_\\___|\\___| .__/ \\___|_|   \n"
+                    + "                                | |              \n"
+                    + "                                |_|              \n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
